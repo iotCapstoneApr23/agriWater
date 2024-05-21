131a2fd2ee3b7c993efe491bc22f1ab73d693431
Wattjames <watt.iii.2010@gmail.com>
2024-05-20 22:23:13 -0400

    Added A shell for Flask app and a index.html for the display of the data.

---
+++ back_end_flask_app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, render_template
+import boto3
+from boto3.dynamodb.conditions import Key
+import datetime
+
+app = Flask(__name__)
+
+# DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+sensor_table = dynamodb.Table('YourSensorTable')
+weather_table = dynamodb.Table('YourWeatherTable')
+sprinkler_table = dynamodb.Table('YourSprinklerTable')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    # Get sensor data from DynamoDB
+    sensor_response = sensor_table.scan()
+    sensor_data = sensor_response['Items']
+
+    # Get weather data from the last hour
+    now = datetime.datetime.utcnow()
+    one_hour_ago = now - datetime.timedelta(hours=1)
+    weather_response = weather_table.query(
+        KeyConditionExpression=Key('timestamp').gte(one_hour_ago.isoformat())
+    )
+    weather_data = weather_response['Items']
+
+    # Get sprinkler data from DynamoDB
+    sprinkler_response = sprinkler_table.scan()
+    sprinkler_data = sprinkler_response['Items']
+
+    return jsonify({
+        'sensor_data': sensor_data,
+        'weather_data': weather_data,
+        'sprinkler_data': sprinkler_data
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
